5159a4f6d909b5d906a0b65d60b604a2b9e3f187
nikitacsk <82165741+nikitacsk@users.noreply.github.com>
2022-05-12 18:12:25 +0300

    new changes

--- task.cs
+++ task.cs
@@ -1,6 +1,7 @@
-string pathacc = "/Users/nikitacsk/Documents/account.txt"; // путь для macOs
-string pathtime = "/Users/nikitacsk/Documents/time.txt";
-string pathinfo = "/Users/nikitacsk/Documents/test.txt";
+
+var pathacc = System.IO.Path.GetFullPath(@"account.txt"); // путь для macOs
+var pathtime = System.IO.Path.GetFullPath(@"time.txt");
+var pathinfo = System.IO.Path.GetFullPath(@"test.txt");
 
 List<Person> children = new List<Person> {};
 string[] infoacc = File.ReadAllLines(pathacc); // считование файла с логином и паролем
@@ -15,48 +16,13 @@ string choice = Console.ReadLine();
 
 if (choice == "Sing in")
 {
-    int index = -1;
-    bool checker = false;
-    while (checker == false) // проверка ли существует такой аккаунт с паролем
-    {
-        Console.Write("Login: ");
-        string login = Console.ReadLine();
-        Console.Write("Password: ");
-        string password = Console.ReadLine();
-        foreach(Person p in children)
-        {
-            index++;
-            if(p.Login == login && p.Password == password)
-            {
-                checker = true;
-                string[] time = File.ReadAllLines(pathtime);
-                time[index * 2] = $"{login} input:{DateTime.Now}";
-                File.WriteAllLines(pathtime, time);
-                break;
-            }
-        }
-        if (checker == false) Console.WriteLine("Wrong login or password");
-    }
+    SingIn child = new SingIn();
 
-    string action = " "; // выбор дейстий на аккаунте просмотреть личную информацию или выйти с системы
-    while (action != "exit")
-    {
-        Console.Write("What do you want do?(view my information or exit)");
-        action = Console.ReadLine();
-        if (action == "view my information")
-        {
-            string[] info = File.ReadAllLines(pathinfo);
-            Console.WriteLine(info[index]);
-        }
-        else if(action != "exit")Console.WriteLine("Unknown command");
-    }
-    if (action == "exit")
-    {
-        string[] time = File.ReadAllLines(pathtime);
-        time[index * 2 + 1] = $"{children[index].Login} output:{DateTime.Now}";
-        File.WriteAllLines(pathtime, time);
-        Console.WriteLine("Finish program");
-    }
+    int index = -1;
+
+    index = child.Entry(pathtime, index, children);
+
+    child.GetInfo(pathtime, pathinfo, index, children);
 }
 else if (choice == "Sing up")
 {
@@ -67,6 +33,7 @@ else if (choice == "Sing up")
         Console.WriteLine("Login must be not null ");
         hlog = Console.ReadLine();
     }
+
     Console.Write("Password: ");
     string? hpas = Console.ReadLine();
     while (hpas == null)
@@ -74,39 +41,15 @@ else if (choice == "Sing up")
         Console.WriteLine("Password must be not null ");
         hpas = Console.ReadLine();
     }
-    children.Add(new Person(hlog, hpas)); // добавление нового пользователя в масив классов и файл с логинами и паролями
-    await File.AppendAllTextAsync(pathacc, hlog);
-    await File.AppendAllTextAsync(pathacc, "\n");
-    await File.AppendAllTextAsync(pathacc, hpas);
-    await File.AppendAllTextAsync(pathacc, "\n");
 
-    Console.Write("Write information about you: "); // добавление личной информации
-    string information = Console.ReadLine();
-    await File.AppendAllTextAsync(pathinfo, information);
-    await File.AppendAllTextAsync(pathinfo,  "\n");
+    Console.Write("Write information about you: ");
+    string? information = Console.ReadLine();
 
-    await File.AppendAllTextAsync(pathtime, $"{hlog} input: {DateTime.Now}");
-    await File.AppendAllTextAsync(pathtime, "\n");
+    SingUn child = new SingUn(hlog, hpas);
 
-    string action = " "; // 
-    while (action != "exit")
-    {
-        Console.WriteLine("What do you want do?(view my information or exit)");
-        action = Console.ReadLine();
-        if (action == "view my information")
-        {
-            string[] info = File.ReadAllLines(pathinfo);
-            Console.WriteLine(info[info.Length - 1]);
-        }
-        else if(action != "exit") Console.WriteLine("Unknown command");
-    }
-    if (action == "exit")
-    {
-        await File.AppendAllTextAsync(pathtime, $"{hlog} output: {DateTime.Now}");
-        await File.AppendAllTextAsync(pathtime, "/n");
-        Console.WriteLine("Finish program");
-    }
+    child.WriteInfo(pathinfo, pathacc, pathtime, information);
 
+    child.GetInfo(pathinfo, pathtime);
 }
 else Console.WriteLine("Unknown  command");
 
@@ -119,4 +62,106 @@ class Person
         Login = login;
         Password = password;
     }
+}
+
+class SingIn
+{
+    public int Entry(string pathtime, int index, List<Person> children)
+    {
+        bool checker = false;
+        while (checker == false) // проверка ли существует такой аккаунт с паролем
+        {
+            Console.Write("Login: ");
+            string login = Console.ReadLine();
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+            foreach (Person p in children)
+            {
+                index++;
+                if (p.Login == login && p.Password == password)
+                {
+                    checker = true;
+                    string[] time = File.ReadAllLines(pathtime);
+                    time[index * 2] = $"{login} input:{DateTime.Now}";
+                    File.WriteAllLines(pathtime, time);
+                    break;
+                }
+            }
+            if (checker == false) Console.WriteLine("Wrong login or password");
+        }
+        return index;
+    }
+
+    public void GetInfo(string pathtime, string pathinfo, int index, List<Person> children)
+    {
+        string action = " "; // выбор дейстий на аккаунте просмотреть личную информацию или выйти с системы
+        while (action != "exit")
+        {
+            Console.Write("What do you want do?(view my information or exit): ");
+            action = Console.ReadLine();
+            if (action == "view my information")
+            {
+                string[] info = File.ReadAllLines(pathinfo);
+                Console.WriteLine(info[index]);
+            }
+            else if (action != "exit") Console.WriteLine("Unknown command");
+        }
+        if (action == "exit")
+        {
+            string[] time = File.ReadAllLines(pathtime);
+            time[index * 2 + 1] = $"{children[index].Login} output:{DateTime.Now}";
+            File.WriteAllLines(pathtime, time);
+            Console.WriteLine("Finish program");
+        }
+    }
+}
+
+class SingUn
+{
+    public string login { get; set; }
+    public string password { get; set; }
+
+    public SingUn(string login, string password)
+    {
+        this.login = login;
+        this.password = password;
+    }
+
+    public void GetInfo(string pathinfo, string pathtime)
+    {
+        string action = " ";
+        while (action != "exit")
+        {
+            Console.WriteLine("What do you want do?(view my information or exit)");
+            action = Console.ReadLine();
+            if (action == "view my information")
+            {
+                string[] info = File.ReadAllLines(pathinfo);
+                Console.WriteLine(info[info.Length - 1]);
+            }
+            else if (action != "exit") Console.WriteLine("Unknown command");
+        }
+        if (action == "exit")
+        {
+            File.AppendAllText(pathtime, $"{login} output: {DateTime.Now}");
+            File.AppendAllText(pathtime, "\n");
+            Console.WriteLine("Finish program");
+        }
+    }
+
+    public void WriteInfo(string pathinfo, string pathacc, string pathtime, string? information)
+    {
+        File.AppendAllText(pathacc, login);
+        File.AppendAllText(pathacc, "\n");
+
+        File.AppendAllText(pathacc, password);
+        File.AppendAllText(pathacc, "\n");
+
+        File.AppendAllText(pathinfo, information);
+        File.AppendAllText(pathinfo, "\n");
+
+        File.AppendAllText(pathtime, $"{login} input: {DateTime.Now}");
+        File.AppendAllText(pathtime, "\n");
+    }
+
 }
